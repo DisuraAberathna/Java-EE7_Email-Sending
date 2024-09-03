75eeb3537205deda698ee4bdf225edcb83ba692f
Disura Chathwara <disura2005@gmail.com>
2024-09-03 09:06:38 +0530

    Create SendMail servlet

---
+++ src/java/controller/SendMail.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author SINGER
+ */
+@WebServlet(name = "SendMail", urlPatterns = {"/SendMail"})
+public class SendMail extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  
+    }
+
+}
