f9cb51b78b630867e907133cfd450f22d988aa45
Disura Chathwara <disura2005@gmail.com>
2024-09-03 10:28:23 +0530

    Update Mail model

--- src/java/model/Mail.java
+++ src/java/model/Mail.java
@@ -18,37 +18,40 @@ import javax.mail.internet.MimeMessage;
  * @author SINGER
  */
 public class Mail {
+
     private static final String APP_EMAIL = "disura2005@gmail.com";
     private static final String APP_PASSWORD = "seguaatyhjahbmvq";
-
+    
     public static void sendMail(String email, String subject, String content) {
-
+        
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2"); 
+        
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(Mail.APP_EMAIL, Mail.APP_PASSWORD);
             }
         });
-
+        
         try {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(Mail.APP_EMAIL));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
             message.setSubject(subject);
-            message.setText(content);
-
+//            message.setText(content);
+            message.setContent("<h1>Hello</h1>", "text/html");
+            
             Transport.send(message);
             System.out.println("Email sent successfully!");
-
+            
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
-
+    
 }
